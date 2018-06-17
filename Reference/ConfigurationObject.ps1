b056cd9079b5398addc0ca419ed8c454e2c7ca82
Matt Small <sperospes@gmail.com>
2018-06-17 11:19:46 -0500

    Reorganized Reference material

---
+++ Reference/ConfigurationObject.ps1
@@ -0,0 +1,30 @@
+@{
+    Companies = @{
+        Company = @{
+            Domain = $false -or @{
+                PSDriveLetter = 'EX'
+                FQDN = 'example.com'
+                PreferedDomainController = $false -or 'DomainController.example.com'
+                Favorite = $true -or $false
+                CredentialName = 'StoredCredentialName' -or $false
+            }
+            OnPremServices = @{
+                ExchangeUri = $false -or 'http://ExchangeServer.example.com/PowerShell/'
+                SkypeUri = $false -or 'https://SkypeFE.example.com/OCSPowerShell'
+                CredentialName = 'StoredCredentialName' -or $false
+            }
+            O365 = $false -or @{
+                Mfa = $true -or $false
+                ExchangeOnlineUri = 'https://outlook.office365.com/powershell-liveid/'
+                SkypeOnlineUri = 'https://online.lync.com/OCSPowerShell'
+                SharePointUri = 'https://<Tenant>-admin.sharepoint.com/'
+                CredentialName = 'StoredCredentialName'
+            }
+        } 
+    }
+    AzureSkuTable = @{
+        'E1' = 'STANDARDPACK'
+        'E3' = 'ENTERPRISEPACK'
+        'E5' = 'ENTERPRISEPREMIUM'
+    }
+}
